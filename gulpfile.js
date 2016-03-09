a88e0a13dcc09be318c3f13277a1fe8225f6fe3d
Lyudmyla <glyuda@yahoo.com>
2016-03-10 01:06:41 +0200

    Add font for header and correct gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -7,13 +7,17 @@
 //all paths we use
     var paths = {
         src: {
+            html: 'source/*.html',
             css: 'source/sass/**/*.scss',
+            css_fonts: 'source/css/fonts/*.*',
             img: 'source/images/**/*.*',
             js: 'source/js/**/*.js'
 
         },
         dest: {
+            html: 'public',
             css: 'public/css',
+            css_fonts: 'public/css/fonts',
             images: 'public/images',
             js: 'public/js'
         }
@@ -21,9 +25,17 @@
 //all tasks we can do
     gulp.task('startWork', function() {
     //all watchers
+        gulp.watch(paths.src.html, function() {
+            gulp.run('htmlMove');
+        });
+
         gulp.watch(paths.src.css, function() {
             gulp.run('cssCreate');
         });
+
+        gulp.watch(paths.src.css_fonts, function() {
+            gulp.run('fontsMove');
+        })
 
         gulp.watch(paths.src.js, function() {
             gulp.run('jsMove')
@@ -35,11 +47,21 @@
 
     })
 
+    gulp.task('htmlMove', function() {
+        gulp.src(paths.src.html)
+            .pipe(gulp.dest(paths.dest.html))
+    });
+
     gulp.task('cssCreate', function() {
         gulp.src(paths.src.css)
             .pipe(sass())
             .pipe(gulp.dest(paths.dest.css))
             //.pipe(plugins.livereload());
+    });
+
+    gulp.task('fontsMove', function() {
+        gulp.src(paths.src.css_fonts)
+            .pipe(gulp.dest(paths.dest.css))
     });
 
     gulp.task('jsMove', function() {
