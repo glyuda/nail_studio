9994df8654cf5645a1d9dea946ab9a1cf216e27d
Lyudmyla <glyuda@yahoo.com>
2016-03-12 01:59:07 +0200

    Add test server

---
+++ server/server.js
@@ -0,0 +1,35 @@
+'use strict';
+
+var Hapi = require('hapi');
+
+var server = new Hapi.Server();
+//add the inert plugin to our Hapi application
+    server.register(require('inert'));
+
+//configurate connection with server
+server.connection({
+    host: 'localhost',
+    port: 7777
+});
+
+//add route
+server.route([
+    {
+        method: 'GET',
+        path: '/{param*}',// * is any file and any nesting;  it's parameter of path
+        handler:{
+            directory: {
+                path: 'public',
+                index: true//with this file we will get only index.html
+            }
+        }
+    }/*,
+    {
+
+    }*/
+]);
+
+//start server
+server.start(function(){
+    console.log('Server started');
+});
